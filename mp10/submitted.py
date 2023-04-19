7178a6360c634acb5b8339586c6605feccaf8a9c
bekaboo <18127878294@qq.com>
2023-04-19 11:28:18 -0500

    mp10: Implement cmopute_transition_matrix()

--- mp10/submitted.py
+++ mp10/submitted.py
@@ -6,6 +6,7 @@ each "raise RuntimeError" line with a line that performs the function specified 
 function's docstring.
 """
 import numpy as np
+import itertools
 
 epsilon = 1e-3
 
@@ -18,7 +19,39 @@ def compute_transition_matrix(model):
     Output:
     P - An M x N x 4 x M x N numpy array. P[r, c, a, r', c'] is the probability that the agent will move from cell (r, c) to (r', c') if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
     """
-    raise RuntimeError("You need to write this part!")
+
+    def reachable(row, col, model):
+        return (
+            row >= 0
+            and row < model.M
+            and col >= 0
+            and col < model.N
+            and not model.W[row, col]
+        )
+
+    transition_matrix = np.zeros((model.M, model.N, 4, model.M, model.N))
+    actions = ([0, -1], [-1, 0], [0, 1], [1, 0])
+    for row, col in itertools.product(range(model.M), range(model.N)):
+        if model.T[row, col]:  # terminal state
+            continue
+        for actionnr in range(4):
+            action = actions[actionnr]
+            for flag in range(3):
+                if flag == 1:
+                    action = actions[(actionnr + 3) % 4]  # rotate left
+                elif flag == 2:
+                    action = actions[(actionnr + 1) % 4]  # rotate right
+                new_row = row + action[0]
+                new_col = col + action[1]
+                if reachable(new_row, new_col, model):
+                    transition_matrix[
+                        row, col, actionnr, new_row, new_col
+                    ] += model.D[row, col, flag]
+                else:
+                    transition_matrix[row, col, actionnr, row, col] += model.D[
+                        row, col, flag
+                    ]
+    return transition_matrix
 
 
 def update_utility(model, P, U_current):
